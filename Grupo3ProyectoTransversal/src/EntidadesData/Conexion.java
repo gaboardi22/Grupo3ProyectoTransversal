ded211a5bcad97031bded487d9d48f49f8c7fa5b
gaboardi22 <andresgaboardi@gmail.com>
2024-09-30 13:08:27 -0300

    .
    clase alumno materia y conexion

---
+++ Grupo3ProyectoTransversal/src/EntidadesData/Conexion.java
@@ -0,0 +1,39 @@
+
+package EntidadesData;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import org.mariadb.jdbc.Connection;
+import org.mariadb.jdbc.*;
+import java.sql.*;
+
+
+/**
+ *
+ * Grupo 3
+ */
+public class Conexion {
+    
+    private static final String URL = "jdbc:mariadb://localhost/";
+    private static final String DB = "universidad";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD  = "";
+    private static Connection connection;     
+    
+    private Conexion(){}
+    
+    public static Connection getConexion(){
+        if(connection != null){
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection = (Connection) DriverManager.getConnection(URL + DB, USUARIO, PASSWORD);
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showConfirmDialog(null, "Error al cargar los drivers");
+            } catch (SQLException ex) {
+                JOptionPane.showConfirmDialog(null, "Error al conectarse a la base de datos");
+            }
+        }
+          return connection;
+    }
+}
---
+++ Grupo3ProyectoTransversal/src/grupo3proyectotransversal/Grupo3ProyectoTransversal.java
@@ -0,0 +1,15 @@
+
+package grupo3proyectotransversal;
+
+/**
+ *
+ * @author Grupo 3
+ */
+public class Grupo3ProyectoTransversal {
+
+   
+    public static void main(String[] args) {
+       
+    }
+    
+}
