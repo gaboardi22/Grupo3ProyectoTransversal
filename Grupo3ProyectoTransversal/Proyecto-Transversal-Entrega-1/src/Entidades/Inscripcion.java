4220df00eaa67837915210719d7e9e8e88b989bc
Ezequiel <130161620+cachissj3@users.noreply.github.com>
2024-10-08 19:56:46 -0300

    todas las entidades agregadas

---
+++ Grupo3ProyectoTransversal/Proyecto-Transversal-Entrega-1/src/Entidades/Inscripcion.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades;
+
+/**
+ *
+ * @author Ezequiel
+ */
+public class Inscripcion {
+    private int idInscripcion;
+    private Alumno alumno;
+    private Materia materia;
+    private double nota;
+
+    public Inscripcion(Alumno alumno, Materia materia, double nota) {
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+    
+}
---
+++ Grupo3ProyectoTransversal/Proyecto-Transversal-Entrega-1/src/Entidades/Materia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades;
+
+/**
+ *
+ * @author Ezequiel
+ */
+public class Materia {
+    private int idMateria;
+    private String nombre;
+    private int anio;
+    private boolean estado;
+}
